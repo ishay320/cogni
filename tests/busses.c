d74f6efeacef86207cd349feee45b2aeb84d57d9
ishay320 <ishay320@hotmail.com>
2023-03-28 21:00:07 +0300

    implemented read values of csv and converted database to full numbers

--- tests/busses.c
+++ tests/busses.c
@@ -1,19 +1,12 @@
 #define COGNI_IMPLEMENTATION
 #include "cogni.h"
 
+#include <stdbool.h>
+
 #define MAX_LINE_SIZE 1024
 
-error get_csv_dimensions(const char* filename, size_t* columns, size_t* rows)
+error get_csv_dimensions(FILE* csv_file, size_t* columns, size_t* rows)
 {
-    FILE* csv_file = fopen(filename, "r");
-    if (csv_file == NULL)
-    {
-        *columns = 0;
-        *rows    = 0;
-        fprintf(stderr, "ERROR: could not open csv file '%s': %s\n", filename, strerror(errno));
-        return 1;
-    }
-
     char c;
     c = (char)fgetc(csv_file);
     if (c == EOF) /* file empty */
@@ -45,7 +38,57 @@ error get_csv_dimensions(const char* filename, size_t* columns, size_t* rows)
     *columns = comma_num + 1;
     *rows    = line_num;
 
-    fclose(csv_file);
+    return 0;
+}
+
+/* uses malloc on data */
+error read_csv_f(const char* filename, float** data, size_t* columns, size_t* rows,
+                 bool throw_first_row)
+{
+    FILE* csv_file = fopen(filename, "r");
+    if (csv_file == NULL)
+    {
+        *columns = 0;
+        *rows    = 0;
+        fprintf(stderr, "ERROR: could not open csv file '%s': %s\n", filename, strerror(errno));
+        return 1;
+    }
+
+    get_csv_dimensions(csv_file, columns, rows);
+    *data = malloc((sizeof **data) * (*columns) * (*rows));
+    if (*data == NULL)
+    {
+        fprintf(stderr, "ERROR: could not malloc data: %s", strerror(errno));
+        return 1;
+    }
+
+    // reset file position
+    rewind(csv_file);
+    if (throw_first_row)
+    {
+        fscanf(csv_file, "%*[^\n]\n");
+    }
+
+    char* buffer = NULL;
+    for (size_t line = 0 + throw_first_row; line < (*rows); line++)
+    {
+        size_t buffer_size = 0;
+        if (getline(&buffer, &buffer_size, csv_file) == -1)
+        {
+            printf("ERROR: could not read line in file: %s\n", filename);
+            return 1;
+        }
+
+        char* buffer_ptr = buffer;
+        for (size_t i = 0; i < *columns; i++)
+        {
+            char* end_num                  = NULL;
+            (*data)[line * (*columns) + i] = strtof(buffer_ptr, &end_num);
+            buffer_ptr                     = end_num + 1;
+        }
+    }
+    free(buffer);
+
     return 0;
 }
 
@@ -55,9 +98,15 @@ int main(int argc, char const* argv[])
     UNUSED(argv);
 
     const char* filename = "busses.csv";
-    size_t x, y;
-    get_csv_dimensions(filename, &x, &y);
+    size_t columns, rows;
+    float* xs;
 
+    if (read_csv_f(filename, &xs, &columns, &rows, false) != 0)
+    {
+        return 1;
+    }
+
+    free(xs);
     printf("\033[32m[+] %s passed\033[0m\n", __FILE__);
     return 0;
 }
